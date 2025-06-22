e91b800466e8c69bc495c791c853a51c36ec33b1
TusharSainiii <itusharsainiii@gmail.com>
2025-06-22 18:18:13 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 71300
+let accountEmail = "itusharsainiii@gmail.com"
+var accountPassword = "1234"
+accountCity = "Muzaffarnagar"
+
+/*var is not used generally because of the block space issue*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
+
+accountEmail = "itushar@gmail.com"
+let accountState;
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
